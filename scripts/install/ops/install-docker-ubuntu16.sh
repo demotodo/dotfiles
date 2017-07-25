2dd9829b977526bb8680713ad68ef11f8cee2c19
bribin <bribin@163.com>
2017-07-25 12:21:07 +0800

    install docker on ubuntu16

--- scripts/install/ops/install-docker-ubuntu16.sh
+++ scripts/install/ops/install-docker-ubuntu16.sh
@@ -5,20 +5,17 @@
 #sudo apt-get -y update
 sudo apt-get -y install apt-transport-https ca-certificates
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# for ubuntu 14.04 LTS
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
-
-# remove old repo
-sudo apt-get -y purge lxc-docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get -y update
 
 # recommended
-sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+#sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
-sudo apt-get -y install docker-engine
+#sudo apt-get -y install docker-engine
+sudo apt-get -y install docker-ce
 
 #sudo service docker start
 #sudo docker run hello-world
